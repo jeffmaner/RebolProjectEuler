4bc785c2deba7cff3ef3d5891c74b24a7536f0ba
Jeff Maner <jeffmaner@yahoo.com>
2015-10-02 15:49:06 -0500

    Add more Usage examples. Accept a single problem number.

--- run.r
+++ run.r
@@ -1,13 +1,19 @@
 REBOL [
   Title: "Project Euler Problem Runner"
-  Usage: "do/args %run.r problems: [<series>]"
+  Usage: { "do/args %run.r problems: 3"
+           "do/args %run.r problems: [1 2 3 4]"
+           "do/args %run.r problems: naturals/to 5"
+           "do/args %run.r problems: naturals/from/to 3 9" }
 ]
 
 solutions: read/lines %Solutions.txt
 
 do load %problems.r
 
-foreach p problems [
+ps: copy []
+ps: either series? problems [ problems ] [ append ps problems ]
+
+foreach p ps [
   s: now/time/precise
   problem: do join "problem" to-string p
   e: now/time/precise
