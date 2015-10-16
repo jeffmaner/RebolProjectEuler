3811c0b8642b4f65679cd2c9546339dcedc798b9
Jeff Maner <jeffmaner@yahoo.com>
2015-10-16 14:33:32 -0500

    Finished Problem 17.

--- problems.r
+++ problems.r
@@ -413,73 +413,51 @@ problem16: func [
 problem17: func [
   "How many letters will be used to write the numbers from 1 to 1000 in words?"
 
-  /local letter-counts s n] [
-  letter-counts: [
-     1 3
-     2 3
-     3 5
-     4 4
-     5 4
-     6 3
-     7 5
-     8 5
-     9 4
-
-    10 3
-    11 6
-    12 6
-    13 8
-    14 8
-    15 7
-    16 7
-    17 9
-    18 8
-    19 8
-
-    20 6
-    30 6
-    40 5
-    50 5
-    60 5
-    70 7
-    80 6
-    90 6
+  /local numbers letter-counts n tens tens-letter-counts s] [
+  numbers: parse "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen" " "
+  letter-counts: copy []
+  for n 1 19 1 [
+    append letter-counts length? numbers/(n)
   ]
 
-  letter-count: func [n [integer!]] [
-    letters: func [n [integer!]] [first select/skip letter-counts n 2]
+  ;; Including "ten" here simplifies access to tens-letter-counts.
+  tens: parse "ten twenty thirty forty fifty sixty seventy eighty ninety" " "
+  tens-letter-counts: copy []
+  for n 10 90 10 [
+    append tens-letter-counts length? tens/(n / 10)
+  ]
+
+  letter-count: func [n [integer!] /local hundred-letters and-letters h r t] [
     hundred-letters: length? "hundred"
     and-letters: length? "and"
 
     if n > 99 [
       h: to-integer n / 100
       r: n // 100
-      either r > 0 [
-        return (letters h) + hundred-letters + and-letters + letter-count r
-      ] [
-        return (letters h) + hundred-letters
-      ]
+      return
+        letter-counts/(h) + hundred-letters + either r > 0
+          [ and-letters + letter-count r ]
+          [ 0 ]
     ]
 
     if n > 19 [
-      t: 10 * to-integer n / 10
+      t: to-integer n / 10
       r: n // 10
-      either r > 0 [
-        return (letters t) + letter-count r
-      ] [
-        return letters n
-      ]
+      return
+        either r > 0
+          [ tens-letter-counts/(t) + letter-count r ]
+          [ tens-letter-counts/(n / 10) ]
     ]
 
-    letters n
+    letter-counts/(n)
   ]
 
   s: 0
-  for n 1 1000 1 [
+  for n 1 999 1 [
     s: s + letter-count n
   ]
 
-  s: s + (letter-count 1) + length? "thousand"
+  s: s + letter-counts/1 + length? "thousand"
 
   s
 ]
